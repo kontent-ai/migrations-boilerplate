69ae5bc837ed840d7ebee2868e35430b8f5708e8
Tom Vorel <tomasv2@kentico.com>
2022-10-05 12:42:51 +0200

    Models from prod

---
+++ models/content-type-snippets/index.ts
@@ -0,0 +1,7 @@
+export * from './metadata';
+export * from './opengraph';
+export * from './sitemap_metadata';
+export * from './hero_kk';
+export * from './hero';
+export * from './umlp_content';
+export * from './content_brief';
